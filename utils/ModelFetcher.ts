297e6815cc2b2626e3a047b745960b83d5e0a345
Daniel Beltran <danilidonbeltran@gmail.com>
2021-05-23 12:12:27 +0200

    Added func docs

--- utils/ModelFetcher.ts
+++ utils/ModelFetcher.ts
@@ -5,11 +5,23 @@ import { ListParams } from "./ValidatorFactory.ts";
  * This class provides methods for fetching models in different ways
  */
 export class ModelFetcher {
+  /**
+   * Returns an instance of the model fetched from the DB by Id.
+   * @param modelClass 
+   * @param id 
+   * @returns 
+   */
   static async fetchById(modelClass: typeof Model, id: number) {
     const model = await modelClass.where("id", id).get() as Model;
     return model;
   }
 
+  /**
+   * Returns an array of instances of the model fetched from the DB.
+   * @param modelClass 
+   * @param params Parameters for pagination
+   * @returns 
+   */
   static fetchList(modelClass: typeof Model, params: ListParams) {
     const query = modelClass.offset((params.page - 1) * params.pageSize);
     query.limit(params.pageSize);
