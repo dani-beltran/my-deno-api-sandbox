895f650ecd8d1fcfecc63e015ae3f4a924d95a69
Daniel Beltran <danilidonbeltran@gmail.com>
2021-09-26 16:35:19 +0200

    added middlewares in controllers

--- app/resources/pet/pet.controllers.ts
+++ app/resources/pet/pet.controllers.ts
@@ -5,55 +5,55 @@ import { PetSchema, Pet } from "./pet.model.ts";
 import { ValidatorFactory } from "../../utils/denodb-extended/validator-factory.ts";
 import { CreatedResponse, UpdatedResponse } from "../../utils/denodb-extended/types.ts";
 
-export async function addPetCtrl(request: Request, response: Response<CreatedResponse>) {
-  await Controller.handleRequest(
-    request,
-    response,
+export async function addPetCtrl(req: Request, res: Response<CreatedResponse>) {
+  await Controller.passRequestToService(
+    req,
+    res,
     petServices.addPet,
     Pet.validator,
   );
 }
 
-export async function getPetCtrl(request: Request, response: Response<PetSchema>) {
-  await Controller.handleRequest(
-    request,
-    response,
+export async function getPetCtrl(req: Request, res: Response<PetSchema>) {
+  await Controller.passRequestToService(
+    req,
+    res,
     petServices.getPet,
     ValidatorFactory.buildGetValidator(),
   );
 }
 
-export async function listPetCtrl(request: Request, response: Response<PetSchema[]>) {
-  await Controller.handleRequest(
-    request,
-    response,
+export async function listPetCtrl(req: Request, res: Response<PetSchema[]>) {
+  await Controller.passRequestToService(
+    req,
+    res,
     petServices.listPet,
     ValidatorFactory.buildListValidator(),
   );
 }
 
-export async function putPetCtrl(request: Request, response: Response<UpdatedResponse>) {
-  await Controller.handleRequest(
-    request,
-    response,
+export async function putPetCtrl(req: Request, res: Response<UpdatedResponse>) {
+  await Controller.passRequestToService(
+    req,
+    res,
     petServices.updatePet,
     ValidatorFactory.buildUpdateValidator(Pet),
   );
 }
 
-export async function patchPetCtrl(request: Request, response: Response<UpdatedResponse>) {
-  await Controller.handleRequest(
-    request,
-    response,
+export async function patchPetCtrl(req: Request, res: Response<UpdatedResponse>) {
+  await Controller.passRequestToService(
+    req,
+    res,
     petServices.updatePet,
     ValidatorFactory.buildPatchValidator(Pet),
   );
 }
 
-export async function deletePetCtrl(request: Request, response: Response<UpdatedResponse>) {
-  await Controller.handleRequest(
-    request,
-    response,
+export async function deletePetCtrl(req: Request, res: Response<UpdatedResponse>) {
+  await Controller.passRequestToService(
+    req,
+    res,
     petServices.deletePet,
     ValidatorFactory.buildDeleteValidator(),
   );
--- app/utils/deno-api/controller.ts
+++ app/utils/deno-api/controller.ts
@@ -9,18 +9,34 @@ import { ApiError } from "./api-error.ts";
 export class Controller {
 
   /**
-   * Method that handles a http request in the default way.
+   * Method that hands off the http request to the service and sends the http
+   * response with the data returned by the service.
+   * If the service throws an exception it will be managed and the response
+   * will contain the corresponding http status code.
+   * If a validator is passed, it will validate the request params, which
+   * are URL params and body or query params.
+   * @param req http request (opine lib)
+   * @param res http response (opine lib)
+   * @param serviceOperation 
+   * @param validator params validator
+   * @param extraParams optional parameters to pass to the service. Useful for passing headers as params. It's recommended to validate then before doing so.
    */
-  static async handleRequest(
+  static async passRequestToService(
     req: Request,
     res: Response,
     serviceOperation: (params: any) => any,
     validator?: (params: any) => any,
+    extraParams?: {[key: string]: number | string | boolean}
   ) {
     try {
-      let params = { ...(req.body || req.query), ...req.params };
+      let params = req.method === 'get' 
+        ? {...req.params, ...req.query } 
+        : {...req.params, ...req.body };
       if (validator) {
         params = Controller.getValidatedParams(req, validator);
+      }
+      if (extraParams) {
+        params = {...params, extraParams};
       }
       const serviceResponse = await serviceOperation(params);
       Controller.sendResponse(res, serviceResponse);
@@ -79,18 +95,53 @@ export class Controller {
 
   /**
    * @returns all possible params of a http request in an object. Including the
-   * body, the query search params, the url params and the headers.
+   * body, the query search params and the url params.
    */
   private static collectRequestParams(req: Request) {
-    const headers = Array.from(req.headers.entries()).reduce(
-      entriesToDictionaryReducer,
-      {},
-    );
     return {
-      ...headers,
       ...req.params,
       ...req.body,
       ...req.query,
     };
+  }
+
+  /**
+   * It run middleware functions. To use within a controller.
+   * If there's an exception, it will be managed and the response sent with 
+   * the corresponding http status code.
+   * @param req 
+   * @param res 
+   * @param middlewareFns 
+   * @returns an array with the results of each middleware function
+   */
+  static runMiddleware(req: Request, res: Response, middlewareFns: ((req: Request, res: Response) => any)[]) {
+    try {
+      return middlewareFns.map((fn) => {
+        return fn(req, res);
+      });
+    } catch(error) {
+      const apiError = new ApiError(error);
+      Controller.sendError(res, apiError);
+      throw error;
+    }
+  }
+
+  /**
+   * @param req 
+   * @param validator
+   * @returns the request headers that passed the validation
+   */
+  static getValidatedHeaders(req: Request, validator: (params: any) => any) {
+    const reqHeaders = Array.from(req.headers.entries()).reduce(
+      entriesToDictionaryReducer,
+      {},
+    );
+    const [error, headers] = validator(reqHeaders);
+    if (error) {
+      error.statusCode = 400;
+      throw error;
+    }
+    // Remove the headers with undefined values
+    return Object.entries(headers).reduce(entriesToDictionaryReducer, {});
   }
 }
