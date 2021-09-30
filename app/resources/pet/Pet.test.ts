4c872a764f987ed763efcac824082fb67193afa9
Daniel Beltran <danilidonbeltran@gmail.com>
2021-09-30 18:47:08 +0200

    Added more Pet tests

--- app/resources/pet/Pet.test.ts
+++ app/resources/pet/Pet.test.ts
@@ -24,7 +24,7 @@ const API_URL = `http://localhost:${PORT}/api`;
 // CREATE Pet
 //////////////////////////////////////////////////////////////////////////////
 const postPetSuite: TestSuite<PetSuiteContext> = new TestSuite({
-  name: "[Post Pet]",
+  name: "Create Pet endpoint",
   sanitizeOps: false,
   sanitizeResources: false,
   beforeAll(context: PetSuiteContext) {
@@ -102,10 +102,10 @@ test(postPetSuite, "should return 200 status code and affect only one row", asyn
   context.petsIds.push(resBody.lastInsertId);
 }); 
 
-test(postPetSuite, "should had created the pet", async (context: PetSuiteContext) => {
-  const petId = context.petsIds[context.petsIds.length - 1];
-  const pet = await PetServices.getPet({id: petId});
-  assertEquals(pet.id, petId);
+test(postPetSuite, "should had created the resource", async (context: PetSuiteContext) => {
+  const lastPetId = context.petsIds[context.petsIds.length - 1];
+  const pet = await PetServices.getPet({id: lastPetId});
+  assertEquals(pet.id, lastPetId);
   assertEquals(pet.name, 'test-dog');
   assertEquals(pet.age,  15);
 });
@@ -114,7 +114,7 @@ test(postPetSuite, "should had created the pet", async (context: PetSuiteContext
 // LIST Pet
 //////////////////////////////////////////////////////////////////////////////
 const listPetSuite: TestSuite<PetSuiteContext> = new TestSuite({
-  name: "[Get Pet]",
+  name: "List Pet endpoint",
   sanitizeOps: false,
   sanitizeResources: false,
   beforeAll(context: PetSuiteContext) {
@@ -248,4 +248,418 @@ test(listPetSuite, "should return elements sorted by age in ascendent order if s
   assertEquals(resBody[1].age, 3);
   assertEquals(resBody[2].age, 4);
   assertEquals(resBody[3].age, 10);
+});
+
+
+// GET Pet
+//////////////////////////////////////////////////////////////////////////////
+
+const getPetSuite: TestSuite<PetSuiteContext> = new TestSuite({
+  name: "Get Pet endpoint",
+  sanitizeOps: false,
+  sanitizeResources: false,
+  beforeAll(context: PetSuiteContext) {
+    console.info = () => {};
+    context.petsIds = [];
+    context.method = 'GET';
+    context.endpointUrl = `${API_URL}/pets`;
+    context.headers = {
+      'Content-Type': 'application/json',
+    };
+    context.testFactory = new IntegrationTestFactory({
+      endpointUrl: context.endpointUrl, 
+      headers: context.headers,
+      method: context.method
+    });
+    context.appServer = new AppServer({port: Number(PORT), env: ENV, flushDB: true});
+    return context.appServer.run().then(() => {
+      const data = [
+        {
+          name: 'animal1',
+          species: Species.Cat,
+          age: 10,
+          insurancePolicy: undefined,
+          description: undefined
+        },
+        {
+          name: 'animal2',
+          species: Species.Dog,
+          age: 2,
+          insurancePolicy: undefined,
+          description: undefined
+        }
+      ];
+      data.forEach(async (pet) => {
+        const res = await PetServices.addPet(pet);
+        context.petsIds.push(res.lastInsertId);
+      });
+    })
+  },
+  afterAll(context: PetSuiteContext) {
+  // No need to remove data from DB since it's flushed at the start of each TestSuite
+  // Just stop the app server
+    return context.appServer.stop();
+  },
+  beforeEach() {
+    // Do nothing
+  },
+  afterEach() {
+    // Do nothing
+  },
+});
+
+test(getPetSuite, "should return 404 not found if pet doesn't exist", async (context: PetSuiteContext) => {
+  const url = `${context.endpointUrl}/999`;
+  const res = await fetch(url, {method: context.method, headers: context.headers});
+  const resBody = await res.text();
+  assertEquals(res.status, 404);
+  assertEquals(resBody, 'Not found');
+});
+
+test(getPetSuite, "should return a pet", async (context: PetSuiteContext) => {
+  const url = `${context.endpointUrl}/2`;
+  const res = await fetch(url, {method: context.method, headers: context.headers});
+  const resBody = await res.json();
+  assertEquals(res.status, 200);
+  assertEquals(resBody.name, 'animal2');
+});
+
+
+
+// UPDATE Pet
+//////////////////////////////////////////////////////////////////////////////
+
+const updatePetSuite: TestSuite<PetSuiteContext> = new TestSuite({
+  name: "Update Pet endpoint",
+  sanitizeOps: false,
+  sanitizeResources: false,
+  beforeAll(context: PetSuiteContext) {
+    console.info = () => {};
+    context.petsIds = [];
+    context.method = 'PUT';
+    context.endpointUrl = `${API_URL}/pets`;
+    context.headers = {
+      'Content-Type': 'application/json',
+    };
+    context.testFactory = new IntegrationTestFactory({
+      endpointUrl: context.endpointUrl, 
+      headers: context.headers,
+      method: context.method
+    });
+    context.appServer = new AppServer({port: Number(PORT), env: ENV, flushDB: true});
+    return context.appServer.run().then(() => {
+      const data = [
+        {
+          name: 'animal1',
+          species: Species.Cat,
+          age: 10,
+          insurancePolicy: undefined,
+          description: undefined
+        },
+        {
+          name: 'animal2',
+          species: Species.Dog,
+          age: 2,
+          insurancePolicy: undefined,
+          description: undefined
+        }
+      ];
+      data.forEach(async (pet) => {
+        const res = await PetServices.addPet(pet);
+        context.petsIds.push(res.lastInsertId);
+      });
+    })
+  },
+  afterAll(context: PetSuiteContext) {
+  // No need to remove data from DB since it's flushed at the start of each TestSuite
+  // Just stop the app server
+    return context.appServer.stop();
+  },
+  beforeEach() {
+    // Do nothing
+  },
+  afterEach() {
+    // Do nothing
+  },
+});
+
+
+test(updatePetSuite, "should return 400 status code if species is missing", async (context: PetSuiteContext) => {
+  const validationTest = context.testFactory.buildBodyValidationTest({paramId: 1});
+  await validationTest({age: 1, name: 'Doggy'}, 'species: Invalid species');
+});
+
+test(updatePetSuite, "should return 400 status code if species is invalid", async (context: PetSuiteContext) => {
+  const validationTest = context.testFactory.buildBodyValidationTest({paramId: 1});
+  await validationTest({age: 1, name: 'Doggy', species: 'unknown/invalid'}, 'species: Invalid species');
+});
+
+test(updatePetSuite, "should return 400 status code if name is too short", async (context: PetSuiteContext) => {
+  const validationTest = context.testFactory.buildBodyValidationTest({paramId: 1});
+  await validationTest({age: 1, name: 'aa', species: 'dog'}, 'name: Expect length to be between 3 and 40 characters (actual: 2)');
+});
+
+test(updatePetSuite, "should return 400 status code if name is too long", async (context: PetSuiteContext) => {
+  const validationTest = context.testFactory.buildBodyValidationTest({paramId: 1});
+  await validationTest({age: 1, name: 'a'.repeat(41), species: 'dog'}, 'name: Expect length to be between 3 and 40 characters (actual: 41)');
+});
+
+test(updatePetSuite, "should return 400 status code if age is less than 0", async (context: PetSuiteContext) => {
+  const validationTest = context.testFactory.buildBodyValidationTest({paramId: 1});
+  await validationTest({age: -1, species: 'dog'}, 'age: Expect value to be greater than 0 (actual: -1)');
+});
+
+test(updatePetSuite, "should return 400 status code if insurancePolicy not complies to format", async (context: PetSuiteContext) => {
+  const validationTest = context.testFactory.buildBodyValidationTest({paramId: 1});
+  await validationTest({age: 12, species: 'dog', insurancePolicy: 'a333'}, 'insurancePolicy: Invalid string format (expected: /^[a-z0-9]{10,64}$/)');
+});
+
+
+test(updatePetSuite, "should return 404 not found if resource doesn't exist", async (context: PetSuiteContext) => {
+  const url = `${context.endpointUrl}/999`;
+  const res = await fetch(url, {
+    method: context.method, 
+    headers: context.headers,
+    body: JSON.stringify({
+      name: '  test-dog  ',
+      age: 15,
+      species: Species.Dog
+    }),
+  });
+  const resBody = await res.text();
+  assertEquals(res.status, 404);
+  assertEquals(resBody, 'Not found');
+});
+
+test(updatePetSuite, "should return 200 and modify only one resource", async (context: PetSuiteContext) => {
+  const url = `${context.endpointUrl}/2`;
+  const res = await fetch(url, {
+    method: context.method, 
+    headers: context.headers,
+    body: JSON.stringify({
+      name: '  test-dog  ',
+      age: 15,
+      species: Species.Dog
+    }),
+  });
+  const resBody = await res.json();
+  assertEquals(res.status, 200);
+  assertEquals(resBody.affectedRows, 1);
+});
+
+test(updatePetSuite, "should had updated the resource", async () => {
+  const pet = await PetServices.getPet({id: 2});
+  assertEquals(pet.id, 2);
+  assertEquals(pet.name, 'test-dog');
+  assertEquals(pet.age,  15);
+});
+
+
+// PATCH Pet
+//////////////////////////////////////////////////////////////////////////////
+
+const patchPetSuite: TestSuite<PetSuiteContext> = new TestSuite({
+  name: "Patch Pet endpoint",
+  sanitizeOps: false,
+  sanitizeResources: false,
+  beforeAll(context: PetSuiteContext) {
+    console.info = () => {};
+    context.petsIds = [];
+    context.method = 'PATCH';
+    context.endpointUrl = `${API_URL}/pets`;
+    context.headers = {
+      'Content-Type': 'application/json',
+    };
+    context.testFactory = new IntegrationTestFactory({
+      endpointUrl: context.endpointUrl, 
+      headers: context.headers,
+      method: context.method
+    });
+    context.appServer = new AppServer({port: Number(PORT), env: ENV, flushDB: true});
+    return context.appServer.run().then(() => {
+      const data = [
+        {
+          name: 'animal1',
+          species: Species.Cat,
+          age: 10,
+          insurancePolicy: undefined,
+          description: undefined
+        },
+        {
+          name: 'animal2',
+          species: Species.Mice,
+          age: 2,
+          insurancePolicy: undefined,
+          description: undefined
+        }
+      ];
+      data.forEach(async (pet) => {
+        const res = await PetServices.addPet(pet);
+        context.petsIds.push(res.lastInsertId);
+      });
+    })
+  },
+  afterAll(context: PetSuiteContext) {
+  // No need to remove data from DB since it's flushed at the start of each TestSuite
+  // Just stop the app server
+    return context.appServer.stop();
+  },
+  beforeEach() {
+    // Do nothing
+  },
+  afterEach() {
+    // Do nothing
+  },
+});
+
+test(patchPetSuite, "should return 400 status code if species is invalid", async (context: PetSuiteContext) => {
+  const validationTest = context.testFactory.buildBodyValidationTest({paramId: 1});
+  await validationTest({age: 1, name: 'Doggy', species: 'unknown/invalid'}, 'species: Invalid species');
+});
+
+test(patchPetSuite, "should return 400 status code if name is too short", async (context: PetSuiteContext) => {
+  const validationTest = context.testFactory.buildBodyValidationTest({paramId: 1});
+  await validationTest({age: 1, name: 'aa', species: 'dog'}, 'name: Expect length to be between 3 and 40 characters (actual: 2)');
+});
+
+test(patchPetSuite, "should return 400 status code if name is too long", async (context: PetSuiteContext) => {
+  const validationTest = context.testFactory.buildBodyValidationTest({paramId: 1});
+  await validationTest({age: 1, name: 'a'.repeat(41), species: 'dog'}, 'name: Expect length to be between 3 and 40 characters (actual: 41)');
+});
+
+test(patchPetSuite, "should return 400 status code if age is less than 0", async (context: PetSuiteContext) => {
+  const validationTest = context.testFactory.buildBodyValidationTest({paramId: 1});
+  await validationTest({age: -1, species: 'dog'}, 'age: Expect value to be greater than 0 (actual: -1)');
+});
+
+test(patchPetSuite, "should return 400 status code if insurancePolicy not complies to format", async (context: PetSuiteContext) => {
+  const validationTest = context.testFactory.buildBodyValidationTest({paramId: 1});
+  await validationTest({age: 12, species: 'dog', insurancePolicy: 'a333'}, 'insurancePolicy: Invalid string format (expected: /^[a-z0-9]{10,64}$/)');
+});
+
+
+test(patchPetSuite, "should return 404 not found if resource doesn't exist", async (context: PetSuiteContext) => {
+  const url = `${context.endpointUrl}/999`;
+  const res = await fetch(url, {
+    method: context.method, 
+    headers: context.headers,
+    body: JSON.stringify({
+      name: '  test-dog  ',
+    }),
+  });
+  const resBody = await res.text();
+  assertEquals(res.status, 404);
+  assertEquals(resBody, 'Not found');
+});
+
+test(patchPetSuite, "should return 200 and update one resource", async (context: PetSuiteContext) => {
+  const url = `${context.endpointUrl}/2`;
+  const res = await fetch(url, {
+    method: context.method, 
+    headers: context.headers,
+    body: JSON.stringify({
+      name: '  test-mice  ',
+    }),
+  });
+  const resBody = await res.json();
+  assertEquals(res.status, 200);
+  assertEquals(resBody.affectedRows, 1);
+});
+
+test(patchPetSuite, "should had updated only one field of the resource", async () => {
+  const pet = await PetServices.getPet({id: 2});
+  assertEquals(pet.id, 2);
+  assertEquals(pet.name, 'test-mice');
+  assertEquals(pet.age,  2);
+  assertEquals(pet.species,  "mice");
+});
+
+
+
+// DELETE Pet
+//////////////////////////////////////////////////////////////////////////////
+
+const deletePetSuite: TestSuite<PetSuiteContext> = new TestSuite({
+  name: "Delete Pet endpoint",
+  sanitizeOps: false,
+  sanitizeResources: false,
+  beforeAll(context: PetSuiteContext) {
+    console.info = () => {};
+    context.petsIds = [];
+    context.method = 'DELETE';
+    context.endpointUrl = `${API_URL}/pets`;
+    context.headers = {
+      'Content-Type': 'application/json',
+    };
+    context.testFactory = new IntegrationTestFactory({
+      endpointUrl: context.endpointUrl, 
+      headers: context.headers,
+      method: context.method
+    });
+    context.appServer = new AppServer({port: Number(PORT), env: ENV, flushDB: true});
+    return context.appServer.run().then(() => {
+      const data = [
+        {
+          name: 'animal1',
+          species: Species.Cat,
+          age: 10,
+          insurancePolicy: undefined,
+          description: undefined
+        },
+        {
+          name: 'animal2',
+          species: Species.Mice,
+          age: 2,
+          insurancePolicy: undefined,
+          description: undefined
+        }
+      ];
+      data.forEach(async (pet) => {
+        const res = await PetServices.addPet(pet);
+        context.petsIds.push(res.lastInsertId);
+      });
+    })
+  },
+  afterAll(context: PetSuiteContext) {
+  // No need to remove data from DB since it's flushed at the start of each TestSuite
+  // Just stop the app server
+    return context.appServer.stop();
+  },
+  beforeEach() {
+    // Do nothing
+  },
+  afterEach() {
+    // Do nothing
+  },
+});
+
+test(deletePetSuite, "should return 404 not found if resource doesn't exist", async (context: PetSuiteContext) => {
+  const url = `${context.endpointUrl}/999`;
+  const res = await fetch(url, {
+    method: context.method, 
+    headers: context.headers,
+  });
+  const resBody = await res.text();
+  assertEquals(res.status, 404);
+  assertEquals(resBody, 'Not found');
+});
+
+test(deletePetSuite, "should return 200 and delete one resource", async (context: PetSuiteContext) => {
+  const url = `${context.endpointUrl}/2`;
+  const res = await fetch(url, {
+    method: context.method, 
+    headers: context.headers,
+  });
+  const resBody = await res.json();
+  assertEquals(res.status, 200);
+  assertEquals(resBody.affectedRows, 1);
+});
+
+test(deletePetSuite, "should had deleted the resource", async () => {
+  try {
+    await PetServices.getPet({id: 2});
+    throw 'Not deleted'
+  } catch(e) {
+    assertEquals(e, { code: "notFound" });
+  }
 });
