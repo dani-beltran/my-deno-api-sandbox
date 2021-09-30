3df4c5cd5cf8d70889276919c99e4c679c400cb6
Daniel Beltran <danilidonbeltran@gmail.com>
2021-09-30 19:16:01 +0200

    fix issue when res payload contains "code" field

--- app/utils/deno-api/controller.ts
+++ app/utils/deno-api/controller.ts
@@ -57,7 +57,7 @@ export class Controller {
     // payload will be an object consisting of a code and a payload. If not customized
     // send 200 and the payload as received in this method.
     //
-    res.setStatus(payload?.code || 200);
+    res.setStatus(payload?.statusCode || 200);
     const responsePayload = payload?.payload !== undefined
       ? payload.payload
       : payload;
