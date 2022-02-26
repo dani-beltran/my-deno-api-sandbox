380c2feba30a6304c98937a495e53cc234080013
Daniel Beltran <danilidonbeltran@gmail.com>
2022-02-26 11:28:35 +0100

    updated dependencies

--- deps.ts
+++ deps.ts
@@ -1,26 +1,26 @@
 export {
   serve,
   Server
-} from "https://deno.land/std@0.107.0/http/server_legacy.ts";
-export { join as pathJoin} from "https://deno.land/std@0.110.0/path/mod.ts";
-export { signal } from "https://deno.land/std@0.110.0/signal/mod.ts";
+} from "https://deno.land/std@0.114.0/http/server_legacy.ts";
+export { join as pathJoin} from "https://deno.land/std@0.127.0/path/mod.ts";
+export { signal } from "https://deno.land/std@0.127.0/signal/mod.ts";
 
 export {
   assertArrayIncludes,
   assertEquals,
-} from "https://deno.land/std@0.110.0/testing/asserts.ts";
+} from "https://deno.land/std@0.127.0/testing/asserts.ts";
 export {
   Database,
   DataTypes,
   Model,
-  SQLite3Connector,
-} from "https://deno.land/x/denodb@v1.0.39/mod.ts";
-export type { Values } from "https://deno.land/x/denodb@v1.0.39/lib/data-types.ts";
+  PostgresConnector,
+} from "https://deno.land/x/denodb@v1.0.40/mod.ts";
+export type { Values } from "https://deno.land/x/denodb@v1.0.40/lib/data-types.ts";
 export { Request as HttpRequest } from "https://deno.land/x/request@1.3.0/request.ts";
 export { test, TestSuite } from "https://deno.land/x/test_suite@0.9.0/mod.ts";
 
 export { config } from "https://deno.land/x/dotenv@v3.0.0/mod.ts";
-export { json, opine, Router } from "https://deno.land/x/opine@1.8.0/mod.ts";
+export { json, opine, Router } from "https://deno.land/x/opine@1.9.1/mod.ts";
 export type {
   Dictionary,
   Opine,
@@ -28,7 +28,7 @@ export type {
   Request,
   Response,
   NextFunction,
-} from "https://deno.land/x/opine@1.8.0/mod.ts";
+} from "https://deno.land/x/opine@1.9.1/mod.ts";
 export { ValidationError } from "https://denoporter.sirjosh.workers.dev/v1/deno.land/x/computed_types@v1.10.2/src/schema/errors.ts";
 import Schema from "https://denoporter.sirjosh.workers.dev/v1/deno.land/x/computed_types@v1.10.2/src/index.ts";
 export { Schema };
